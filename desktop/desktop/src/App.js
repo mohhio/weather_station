cb887154b3cbb873b831ad993104da5759771906
Konrad Hanus <konrad.hanus@intive.com>
2019-09-28 19:05:40 +0200

    frontend added

--- desktop/desktop/src/App.js
+++ desktop/desktop/src/App.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const dodaj = (event)=>{
+    event.preventDefault();
+    alert('dodalem');
+    axios.post('http://localhost:8080/api/temperature', {
+      value: '10.10',
+      dataTime: '123'
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  const odczytaj = (event)=>{
+    event.preventDefault();
+    axios.get('http://localhost:8080/api/temperature')
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -16,7 +43,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <div onClick={(e)=>dodaj(e)}>Dodaj</div>
+          <div onClick={(e)=>odczytaj(e)}>Odczytaj</div>
         </a>
       </header>
     </div>
