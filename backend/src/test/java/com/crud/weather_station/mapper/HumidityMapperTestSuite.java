11f24994baa95fb7e1f2b33a9282b35c8fcf0a52
Maciej <m.dziagacz@gmail.com>
2019-10-05 20:19:42 +0200

    tests for mappers added

---
+++ backend/src/test/java/com/crud/weather_station/mapper/HumidityMapperTestSuite.java
@@ -0,0 +1,67 @@
+package com.crud.weather_station.mapper;
+
+import com.crud.weather_station.domain.Humidity;
+import com.crud.weather_station.domain.HumidityDto;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HumidityMapperTestSuite {
+    @Autowired
+    private HumidityMapper humidityMapper;
+
+    @Test
+    public void testMapToHumidity() {
+        //Given
+        HumidityDto humidityDto = new HumidityDto(1L, "test", 1.0);
+        //When
+        Humidity humidity = humidityMapper.mapToHumidity(humidityDto);
+        //Then
+        assertNotNull(humidity);
+        assertEquals(humidityDto.getId(), humidity.getId());
+        assertEquals(humidityDto.getDateTime(), humidity.getDateTime());
+        assertEquals(humidityDto.getValue(), humidity.getValue(), 0);
+    }
+
+    @Test
+    public void testMapToHumidityDto() {
+        //Given
+        Humidity humidity = new Humidity(1L, "test", 1.0);
+        //When
+        HumidityDto humidityDto = humidityMapper.mapToHumidityDto(humidity);
+        //Then
+        assertNotNull(humidityDto);
+        assertEquals(humidity.getId(), humidityDto.getId());
+        assertEquals(humidity.getDateTime(), humidityDto.getDateTime());
+        assertEquals(humidity.getValue(), humidityDto.getValue(), 0);
+    }
+
+    @Test
+    public void mapToHumidityDtoList() {
+        //Given
+        List<Humidity> emptyHumidityList = new ArrayList<>();
+        Humidity humidity = new Humidity(1L, "test", 1.0);
+        List<Humidity> humidityList = Arrays.asList(humidity);
+        //When
+        List<HumidityDto> emptyHumidityDtoList = humidityMapper.mapToHumidityDto(emptyHumidityList);
+        List<HumidityDto> humidityDtoList = humidityMapper.mapToHumidityDto(humidityList);
+        //Then
+        assertNotNull(emptyHumidityDtoList);
+        assertNotNull(humidityDtoList);
+        assertEquals(emptyHumidityList.size(), emptyHumidityDtoList.size());
+        assertEquals(humidity.getId(), humidityDtoList.get(0).getId());
+        assertEquals(humidity.getDateTime(), humidityDtoList.get(0).getDateTime());
+        assertEquals(humidity.getValue(), humidityDtoList.get(0).getValue(), 0);
+    }
+}
