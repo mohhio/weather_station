f5fa41f0c31741eb2149456f14aad0d6fe83a5e4
Konrad Hanus <konrad.hanus@intive.com>
2019-10-06 11:37:30 +0200

    some bug for kettle

--- mobile/src/Kettle/saga.js
+++ mobile/src/Kettle/saga.js
@@ -3,7 +3,7 @@ import { eventChannel, buffers, END } from 'redux-saga';
 import { takeEvery, put, call, take, cancelled, all, fork } from 'redux-saga/effects';
 import { actionCreator as log } from './../Log/action';
 import base64 from 'react-native-base64';
-
+import {mixA, mixB, cipher} from '../micypher';
 export function* onConnectSaga(action)
 {
 	const device = action.payload;
@@ -72,17 +72,62 @@ export function* onConnectSaga(action)
 	// service4
 	const characteristics4 = yield call([ manager, manager.characteristicsForDevice ], service4.deviceID, service4.uuid);
 	console.log("CHARAKTERYSTYKA dla serwisu 4:", service4.uuid);
-	console.log();
-	const characteristic = characteristics4[0];
-	const reversedMac = base64.encode("0x57 0x8A 0x33 0x6F 0x7C 0xB8");
+	console.log("CZWARTA CHARAKTERSYTKA", characteristics4);
+	const authInitCharacteristic = characteristics4[3];
+	const authCharacteristic = characteristics4[0];
+	const macAddres = "B8:7C:6F:33:8A:57";
+	const reversedMac = macAddres.split(":").map(s => parseInt(s, 16)).reverse();
+	console.log("reversed mac", reversedMac);
 	const send1 = "0x90, 0xCA, 0x85, 0xDE";
 	const productId = '131';
 	const token = "37 c8 2b 4b c5 9c 83 09 de f0 13 9c";
+	cipherHash = cipher(mixA(reversedMac, productId), token)
 	// const cipher; 
 	// const mixA;
 	// const mixB;
-	const writeCharacteristicWithResponseForDevice = yield call([ manager, manager.writeCharacteristicWithResponseForDevice], characteristic.deviceID, characteristic.serviceUUID, characteristic.uuid, send1);
+
+	//1. Send 0x90, 0xCA, 0x85, 0xDE bytes to authInitCharacteristic
+	console.log(authInitCharacteristic);
+	const writeCharacteristicWithResponseForDevice = yield call([ manager, manager.writeCharacteristicWithResponseForDevice], authCharacteristic.deviceID, authCharacteristic.serviceUUID, authCharacteristic.uuid, send1);
 	console.log(base64.decode(writeCharacteristicWithResponseForDevice.value));
+	
+
+	//2. Subscribe authCharacteristic.
+	const channel = yield eventChannel((emit) => {
+		let emitting = true;
+		authCharacteristic._manager.monitorCharacteristicForDevice(
+			authCharacteristic.deviceID,
+			authCharacteristic.serviceUUID,
+			authCharacteristic.uuid,
+			(error, char) => {
+				try {
+					if (char.value !== null) {
+						if (emitting) {
+							// const temp = base64.decode(char.value);
+							console.log('emit>>>', char);
+							// emit([ error, temp ]);
+							//emitting = false;
+						} else {
+							emit(END);
+						}
+					}
+				} catch (e) {
+					console.log('e:', e);
+					console.log('error', error);
+					emit(END);
+				}
+			}
+		);
+		return () => {
+			console.log('sie rozlaczylo');
+			// manager.stopDeviceScan();
+		};
+	}, buffers.expanding(1));
+
+	//3. Send cipher(mixA(reversedMac, productID), token) to authCharacteristic
+	const cipherResponse = yield call([ manager, manager.writeCharacteristicWithResponseForDevice], authCharacteristic.deviceID, authCharacteristic.serviceUUID, authCharacteristic.uuid, cipherHash);
+	console.log("WCH", cipherResponse);
+
 	//end authentication
 
 
