a097ef3cb61b8daa7d9962036dab5164fc110cc7
Konrad Hanus <konrad.hanus@intive.com>
2019-10-07 21:22:48 +0200

    line chart with temparature added

--- desktop/src/App.js
+++ desktop/src/App.js
@@ -1,54 +1,143 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import ReactApexChart from 'react-apexcharts';
+import moment from 'moment';
+class App extends React.Component {
+	constructor(props) {
+		super(props);
 
-function App() {
+		this.state = {
+			options: {
+				chart: {
+					shadow: {
+						enabled: true,
+						color: '#000',
+						top: 18,
+						left: 7,
+						blur: 10,
+						opacity: 1
+					},
+					toolbar: {
+						show: false
+					}
+				},
+				colors: [ '#77B6EA', '#545454' ],
+				dataLabels: {
+					enabled: true
+				},
+				stroke: {
+					curve: 'smooth'
+				},
+				title: {
+					text: 'Average High & Low Temperature',
+					align: 'left'
+				},
+				grid: {
+					borderColor: '#e7e7e7',
+					row: {
+						colors: [ '#f3f3f3', 'transparent' ], // takes an array which will be repeated on columns
+						opacity: 0.5
+					}
+				},
+				markers: {
+					size: 6
+				},
+				xaxis: {
+					categories: [ '1', '2','3','4','5','6','7','8','9','10' ],
+					title: {
+						text: 'Miesiąc'
+					}
+				},
+				yaxis: {
+					title: {
+						text: 'Temperatura'
+					},
+					min: 5,
+					max: 40
+				},
+				legend: {
+					position: 'top',
+					horizontalAlign: 'right',
+					floating: true,
+					offsetY: -25,
+					offsetX: -5
+				}
+			},
+			series: [
+			
+			]
+		};
+	}
 
-  const dodaj = (event)=>{
-    event.preventDefault();
-    alert('dodalem');
-    axios.post('http://localhost:8080/api/temperature', {
-      value: '10.10',
-      dataTime: '123'
-    })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+	odczytaj = () => {
+
+		axios
+			.get('http://localhost:8080/api/temperature')
+			.then((response) => {
+       // const state = this.state;
+				const data = response.data.map((value) => value.value);
+        const date = response.data.map((value) => moment(new Date(+value.dateTime)).format("h:mm:ss "));
+        const obj = {
+          ...this.state.options, 
+          xaxis: {
+					categories: date,
+					title: {
+						text: 'Miesiąc'
+					}
+        }
+      };
+				this.setState({
+          series: [ { name: 'Temperatura', data } ], 
+          options: obj
+				});
+			})
+			.catch((error) => {
+				console.log(error);
+      });
+      
+      axios
+			.get('http://localhost:8080/api/humidity')
+			.then((response) => {
+       // const state = this.state;
+        const data = response.data.map((value) => value.value);
+				this.setState({
+          series: [...this.state.series, { name: 'Wigotność', data }  ],
+				});
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+
+  componentDidMount(){
+    this.odczytaj();
   }
+	render() {
+		return (
+			<div>
 
-  const odczytaj = (event)=>{
-    event.preventDefault();
-    axios.get('http://localhost:8080/api/temperature')
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div onClick={(e)=>dodaj(e)}>Dodaj</div>
-          <div onClick={(e)=>odczytaj(e)}>Odczytaj</div>
-        </a>
-      </header>
-    </div>
-  );
+				<div id="chart">
+					<ReactApexChart
+						options={this.state.options}
+						series={this.state.series}
+						type="line"
+						width={900}
+						height={420}
+					/>
+				</div>
+
+				{/* <LineChart
+            sets={[
+              arrayOfValue,
+            ]}
+            times={[
+               arrayOfDate.map((e) => new Date(e))
+            ]}
+        /> */}
+			</div>
+		);
+	}
 }
 
 export default App;
